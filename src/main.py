606134e1e3446648ab73beb5f7696d7d53fc144b
AndyDu01 <andy255060@gmail.com>
2022-11-22 14:53:46 +0800

    V1 complete.

--- src/main.py
+++ src/main.py
@@ -4,16 +4,16 @@ import torch
 import torch.optim as optim
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-from tqdm import trange
+from tqdm import trange, tqdm
 from transformers import (
     LongformerTokenizerFast,
-    LongformerModel,
     get_linear_schedule_with_warmup,
 )
-from data.dataset import RelationalDataset
+from data import RelationalDataset
 from utils import set_seed
 import json
 from model import RelationalModel
+
 
 def get_args() -> argparse.Namespace:
     """
@@ -29,7 +29,7 @@ def get_args() -> argparse.Namespace:
         "--device",
         type=torch.device,
         help="cpu, cuda, cuda:0, cuda:1",
-        default="cuda:4",
+        default="cuda",
     )
 
     # Data settings
@@ -37,20 +37,21 @@ def get_args() -> argparse.Namespace:
         "--data_path", default="../data/data_v1.json", type=str)
     parser.add_argument("--max_length", default=2048, type=int)
     parser.add_argument("--batch_size", type=int, default=1)
-    parser.add_argument("--pretrained", type=str, default="allenai/longformer-base-4096")
+    parser.add_argument("--pretrained", type=str,
+                        default="allenai/longformer-base-4096")
 
     # Train valid split
     parser.add_argument("--valid_size", default=0.2, type=float)
 
     # Training settings
-    parser.add_argument("--num_epoch", type=int, default=1)
+    parser.add_argument("--num_epoch", type=int, default=10)
     parser.add_argument("--logging_step", type=int, default=100)
     parser.add_argument("--gradient_accumulation_step", type=int, default=2)
     parser.add_argument("--lr", type=float, default=3e-5)
 
     # Model settings
     parser.add_argument("--num_classes", type=int, default=2)
-    
+
     args = parser.parse_args()
 
     return args
@@ -84,9 +85,10 @@ def main(args) -> None:
     tokenizer = LongformerTokenizerFast.from_pretrained(args.pretrained)
     model = RelationalModel(args.pretrained, args.num_classes)
     model = model.to(device)
-    
+
     # Prepare Dataset and Dataloader
-    train_set = RelationalDataset(train_data, tokenizer, "train", args.max_length)
+    train_set = RelationalDataset(
+        train_data, tokenizer, "train", args.max_length)
     train_loader = DataLoader(
         train_set,
         batch_size=args.batch_size,
@@ -105,7 +107,7 @@ def main(args) -> None:
     logging_step = args.logging_step
     learning_rate = args.lr
     gradient_accumulation_step = args.gradient_accumulation_step
-
+    criterion = torch.nn.BCELoss()
     optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
     '''
     update_step = (
@@ -115,24 +117,45 @@ def main(args) -> None:
         optimizer, 0.1 * update_step, update_step
     )
     '''
-    
+    label1 = torch.concat([torch.ones((args.batch_size, 1)),
+                           torch.zeros((args.batch_size, 1))], dim=1).to(device)
+    label2 = torch.concat([torch.zeros((args.batch_size, 1)),
+                           torch.ones((args.batch_size, 1))], dim=1).to(device)
     # Start training
     epoch_pbar = trange(args.num_epoch, desc="Epoch")
     #step = 0
+
     for epoch in epoch_pbar:
         model.train()
-        for (q_r_seqs, q_ans, r_q_seqs, r_ans) in train_loader:
-            for a,b,c,d in zip(q_r_seqs, q_ans, r_q_seqs, r_ans):
-                a = a.to(device)
-                output = model(a)
-                print(output)
-                break
+        total_loss = 0.0
+        for step, (q_r_seqs, q_ans, r_q_seqs, r_ans) in enumerate(train_loader):
+            for q_input, q_label, r_input, r_label in zip(q_r_seqs, q_ans, r_q_seqs, r_ans):
+                q_input, q_label, r_input, r_label = q_input.to(device), q_label.to(
+                    device), r_input.to(device), r_label.to(device)
+                optimizer.zero_grad()
+                output: torch.Tensor = model(q_input)
+                loss = criterion(output, label1 if q_label else label2)
+                total_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+
+                optimizer.zero_grad()
+                output: torch.Tensor = model(r_input)
+                loss = criterion(output, label1 if r_label else label2)
+                total_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+            tqdm.write("Epoch: [{}/{}], Loss: {:.6f}".format(epoch,
+                       args.num_epoch, total_loss / 2 / (step + 1)))
+        torch.save(model.state_dict(), "/tmp2/b08902011/ckpt/tmp.ckpt")
+
         '''
         if step % gradient_accumulation_step == 0:
             optimizer.step()
             optimizer.zero_grad()
             scheduler.step()
         '''
+
 
 if __name__ == "__main__":
     args = get_args()
--- src/model.py
+++ src/model.py
@@ -4,16 +4,19 @@ import torch.nn as nn
 
 
 class RelationalModel(nn.Module):
-    def __init__(self, pretrained_name, num_classes, **kwargs):
+    def __init__(self, pretrained_name, num_classes=2, **kwargs):
         super(RelationalModel, self).__init__()
         self.upstream = LongformerModel.from_pretrained(pretrained_name)
         input_dim = self.upstream.config.hidden_size
         self.linear = nn.Linear(input_dim, num_classes)
+        self.last = nn.Sigmoid()
 
     def forward(self, input_ids):
-        
+
         upstream_outputs = self.upstream(input_ids)
-        cls_token_states = upstream_outputs.last_hidden_state[:,0,:]
-        
+        cls_token_states = upstream_outputs.last_hidden_state[:, 0, :]
+
         predicted = self.linear(cls_token_states)
+        predicted = self.last(predicted)
+
         return predicted
